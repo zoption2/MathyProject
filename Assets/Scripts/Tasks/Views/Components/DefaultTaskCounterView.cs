62bb8f188a68cbd53a28a9c48f1c4fac9fbf20d1
Vitali Oreletskii <zoption234@gmail.com>
2023-03-28 16:47:14 +0300

    Animation for counter added

--- Assets/Scripts/Tasks/Views/Components/DefaultTaskCounterView.cs
+++ Assets/Scripts/Tasks/Views/Components/DefaultTaskCounterView.cs
@@ -2,6 +2,9 @@
 using UnityEngine.UI;
 using UnityEngine;
 using TMPro;
+using DG.Tweening;
+using UnityEditor.Tilemaps;
+
 
 namespace Mathy.UI
 {
@@ -21,14 +24,38 @@ namespace Mathy.UI
         private const int kCorrectSpriteIndex = 2;
         private const int kWrongSpriteIndex = 3;
         private const string kCounterFormat = "{0}/{1}";
+        private const float kTweenDuration = 0.5f;
+        private const float kSmallOffset = 160;
+        private const float kMediumOffset = 250;
+        private const float kLargeOffset = 340;
+        private readonly Vector3 scaleTo = new Vector3(0.05f, 0.1f, 0);
 
         [SerializeField] private TMP_Text counterText;
+        [SerializeField] private Button button;
+        [SerializeField] private RectTransform movableRect;
+        [SerializeField] private FlexibleGridLayout indicatorPanel;
+        [SerializeField] private List<AudioClip> panelSounds;
         [SerializeField] private Image[] indicators;
         [SerializeField] private Sprite[] statusSprites;
 
         private bool isInited;
+        private bool isOpened;
         private int totalIndicators;
+        private float offsetY;
 
+        private Transform TweenID => transform;
+        private bool IsTweening => DOTween.IsTweening(TweenID);
+
+        private void OnEnable()
+        {
+            button.onClick.AddListener(OnButtonClick);
+        }
+
+        private void OnDisable()
+        {
+            button.onClick.RemoveListener(OnButtonClick);
+            DOTween.Kill(TweenID);
+        }
 
         public void Init(int total, List<bool> existingAnswers = null)
         {
@@ -51,6 +78,7 @@ namespace Mathy.UI
                     : statusSprites[kWrongSpriteIndex];
             }
 
+            offsetY = GetOffsetForMode(total);
             isInited = true;
         }
 
@@ -86,6 +114,55 @@ namespace Mathy.UI
                         , status, typeof(DefaultTaskCounterView))
                         );
             }
+        }
+
+        private void OnButtonClick()
+        {
+            if (!IsTweening)
+            {
+                DoMove();
+            }
+        }
+
+        private void DoMove()
+        {
+            DOTween.Kill(TweenID);
+            var sequence = DOTween.Sequence();
+
+            if (isOpened)
+            {
+                sequence.Append(movableRect.DOShakeScale(kTweenDuration, scaleTo, 10, 60f)).SetId(TweenID);
+                sequence.Join(movableRect.DOAnchorPos(new Vector2(0, 0), kTweenDuration).SetEase(Ease.OutBack)).SetId(TweenID);
+                AudioSystem.Instance.PlaySound(panelSounds[0], UnityEngine.Random.Range(0.9f, 1.1f));
+            }
+            else
+            {
+                sequence.Append(movableRect.DOShakeScale(kTweenDuration, scaleTo, 10, 60f)).SetId(TweenID);
+                sequence.Join(movableRect.DOAnchorPos(new Vector2(0, offsetY), kTweenDuration).SetEase(Ease.OutBack)).SetId(TweenID);
+                AudioSystem.Instance.PlaySound(panelSounds[1], 0.5f, UnityEngine.Random.Range(0.9f, 1.1f));
+            }
+            isOpened = !isOpened;
+        }
+
+        private float GetOffsetForMode(int amount)
+        {
+            float result = kSmallOffset;
+            switch (amount)
+            {
+                case 10:
+                    indicatorPanel.padding.bottom = 390;
+                    result = kSmallOffset;
+                    break;
+                case 20:
+                    indicatorPanel.padding.bottom = 304;
+                    result = kMediumOffset;
+                    break;
+                case 30:
+                    indicatorPanel.padding.bottom = 218;
+                    result = kLargeOffset;
+                    break;
+            }
+            return result;
         }
     }
 }
