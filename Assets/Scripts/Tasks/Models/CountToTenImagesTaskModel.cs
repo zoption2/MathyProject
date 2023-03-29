5bc8d8a729d29032ffa33ed3df759132da38f745
Vitali Oreletskii <zoption234@gmail.com>
2023-03-29 16:56:07 +0300

    CountToTen model added

---
+++ Assets/Scripts/Tasks/Models/CountToTenImagesTaskModel.cs
@@ -0,0 +1,63 @@
+﻿using Mathy.Data;
+using System;
+using System.Collections.Generic;
+
+namespace Mathy.Core.Tasks
+{
+    public interface ICountToTenImagesTaskModel : ITaskModel
+    {
+        int CountToShow { get; }
+    }
+
+
+    public class CountToTenImagesTaskModel : BaseTaskModel
+    {
+        private const int kMaxValueLimit = 10;
+
+        private int countToShow;
+        private List<string> variants;
+        private List<string> operators;
+        private List<string> elements;
+        private List<int> correctIndexes;
+
+        int CountToShow { get; }
+
+
+        public CountToTenImagesTaskModel(ScriptableTask taskSettings) : base(taskSettings)
+        {
+            var random = new System.Random();
+            countToShow = random.Next(kMaxValueLimit);
+
+            elements = new List<string>() 
+            { 
+                countToShow.ToString() 
+            };
+
+            correctIndexes = new List<int>()
+            {
+                countToShow - 1
+            };
+
+            operators = new List<string>()
+            {
+                "="
+            };
+
+            variants = new List<string>()
+            {
+                "1","2","3","4","5","6","7","8","9","10"
+            };
+        }
+
+        public override TaskData GetResult()
+        {
+            var result = new TaskData();
+            result.TaskType = TaskType;
+            result.ElementValues = elements;
+            result.OperatorValues = operators;
+            result.VariantValues = variants;
+            result.CorrectAnswerIndexes = correctIndexes;
+            return result;
+        }
+    }
+}
